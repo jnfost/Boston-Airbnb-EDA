246c7d6a0c347e3c6047e09d94c1de0c1fbb94cc
AGPalmer88 <61656034+AGPalmer88@users.noreply.github.com>
2020-09-01 22:21:19 -0400

    Updated Tableau Pie chart
    South End Property Type by Price and Ratings

--- propertytype.js
+++ propertytype.js
@@ -1,19 +1,41 @@
-Plotly.d3.csv('final_neighborhood_data.json',function(err, rows) 
-{ function unpack(rows, key) {
-    return rows.map(function(row) {return row[key]})
-  }
+// set the dimensions and margins of the graph
+var margin = {top: 40, right: 150, bottom: 60, left: 70},
+    width = 700 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#pricebypropertytype")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.json("final_neighborhood_data.json", function(data) {
+
+var data = [{
+    type: "sunburst",
+    ids: [
+      "Apartments", "House", "Condominium", "Other", "Bed & Breakfast",
+      "Townhouse", "Loft", "Boat"
+    ],
+    labels: [
+      "Apartments", "House", "Condominium", "Other", "Bed<br>&<br>Breakfast", "Townhouse", "Loft", "Boat"
+    ],
+    parents: [
+      "", "", "", "Apartments", "House", "Condominium", "Other",
+      "Bed & Breakfast", "Townhouse","Loft", "Boat"
+    ],
+    outsidetextfont: {size: 20, color: "#377eb8"},
+    // leaf: {opacity: 0.4},
+    marker: {line: {width: 2}},
+  }];
   
-    var data = [{
-          type: "sunburst",
-          maxdepth: 2,
-          ids: unpack(rows, 'ids'),
-          labels: unpack(rows, 'labels'),
-          parents: unpack(rows, 'parents'),
-          textposition: 'inside',
-          insidetextorientation: 'radial'
-    }]
-  
-    var layout = {margin: {l: 0, r: 0, b: 0, t:0}}
-  
-    Plotly.newPlot('myDiv', data, layout)
-  });
+  var layout = {
+    margin: {l: 0, r: 0, b: 0, t:0},
+    sunburstcolorway:["#636efa","#ef553b","#00cc96"],
+  };
+  Plotly.newPlot('pricebypropertytype', data, layout);
+})
